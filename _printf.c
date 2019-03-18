37862673504effe6d06b8c69c8035328d4f25d7f
David Latorre <david@latorredev.com>
2019-03-18 16:23:25 -0500

    add _putchar function to the header file

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /*
 * _printf - entry point
 */
@@ -7,11 +8,45 @@
 int _printf(const char *format, ...)
 {
 	va_list stringArray;
-	int counter;
+	unsigned int counter = 0;
+	char* s;
 
-	va_start(stringArray, counter);
-
+	va_start(stringArray, format);
+	while (format != 0 && format[counter] != '\0')
+	{
+		if (format[counter] == '%')
+		{
+			counter++;
+			
+			switch (format[counter])
+			{
+				case 'c':
+					putchar(va_arg(stringArray, int));
+					break;
+					
+				case 'd':
+					printf("%d",va_arg(stringArray, int));
+					break;
+				case 'i':
+					printf("%i",va_arg(stringArray, int));
+					break;
+				case 's':
+					s = va_arg(stringArray, char*);
+					if (s == NULL || *s == '\0')
+					{
+						printf("nil");
+						break;
+					}
+					printf("%s", s);
+					break;
+				default:
+					break;
+			}
+			counter++;
+		}
+		putchar(format[counter]);
+		counter++;
+	}
 	va_end(stringArray);
- 
-	return (0);
+	return (counter);
 }

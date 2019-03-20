ed91935a0da2fa86148452fb6a14a1f9ac547ec4
Ryan Hudson <ryan.e.hudson@gmail.com>
2019-03-20 00:56:20 +0000

    Fix line length

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdarg.h>
+#define N "(NULL)"
 /**
  * _printf - entry point
  * @format: entry array
@@ -8,43 +9,43 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list stringArray;
-	unsigned int counter = 0;
-	char *s;
-	int nc = 0, c = 0;
+va_list stringArray;
+unsigned int counter = 0;
+char *s;
+int nc = 0, c = 0;
 
-	va_start(stringArray, format);
-	while (format != 0 && format[counter] != '\0')
+va_start(stringArray, format);
+while (format != 0 && format[counter] != '\0')
+{
+	if (format[counter] == '%' && format[counter + 1] != '\0')
 	{
-		if (format[counter] == '%' && format[counter + 1] != '\0')
-		{
-			counter++;
-			switch (format[counter])
-			{
-				case 'c':
-					nc += ((c = va_arg(stringArray, int)) == '\0') ? 1 : _putchar(c);
-					break;
-				case 'd':
-					nc += _putint(va_arg(stringArray, int));
-					break;
-				case 'i':
-					nc += _putint(va_arg(stringArray, int));
-					break;
-				case 's':
-					nc += ((s = va_arg(stringArray, char*)) == NULL) ? _puts("(null)") : _puts(s);
-					break;
-				default:
-					nc += _putchar('%');
-					nc += (format[counter] == '%') ? 0 : _putchar(format[counter]);
-					break;
-			}
-		}
-		else
-			nc += (format[counter] == '%') ? -1 : _putchar(format[counter]);
-		if (format[counter] == 0)
-			_putchar(0);
 		counter++;
+		switch (format[counter])
+		{
+			case 'c':
+				nc += ((c = va_arg(stringArray, int)) == '\0') ? 1 : _putchar(c);
+				break;
+			case 'd':
+				nc += _putint(va_arg(stringArray, int));
+				break;
+			case 'i':
+				nc += _putint(va_arg(stringArray, int));
+				break;
+			case 's':
+				nc += ((s = va_arg(stringArray, char*)) == NULL) ? _puts(N) : _puts(s);
+				break;
+			default:
+				nc += _putchar('%');
+				nc += (format[counter] == '%') ? 0 : _putchar(format[counter]);
+				break;
+		}
 	}
-	va_end(stringArray);
-	return ((nc == 0) ? -1 : nc);
+	else
+		nc += (format[counter] == '%') ? -1 : _putchar(format[counter]);
+	if (format[counter] == 0)
+		_putchar(0);
+	counter++;
+}
+va_end(stringArray);
+return ((nc == 0) ? -1 : nc);
 }
